b63e68cb5644e5ea4a070b72f8fa9e0897f7d162
Israel Herraiz <ihr@google.com>
2022-01-08 19:49:41 +0100

    WIP

--- trainer/task.py
+++ trainer/task.py
@@ -12,74 +12,11 @@ template. Please feel free to remove this, or the other
 (sklearn_main.py), or adapt as you need.
 """
 
-
-def add(x, y):
-    """Add the given parameters.
-
-    :param x: x value
-    :type x: int
-    :param y: y value
-    :type y: int
-    :return: the sum of x and y
-    :rtype: int
-    """
-    return x + y
-
-
-def subtract(x, y):
-    """Substract the given parameters.
-
-    :param x: x value
-    :type x: int
-    :param y: y value
-    :type y: int
-    :return: the diff of x and y
-    :rtype: int
-    """
-    return x - y
-
-
-def multiply(x, y):
-    """Multiply the given parameters.
-
-    :param x: x value
-    :type x: int
-    :param y: y value
-    :type y: int
-    :return: the multiplication of x and y
-    :rtype: int
-    """
-    return x * y
-
-
-def divide(x, y):
-    """Divide the given parameters.
-
-    :param x: x value
-    :type x: int
-    :param y: y value
-    :type y: int
-    :return: the division of x by y
-    :rtype: int
-    """
-    return x / y
-
+def train_and_
 
 def main():
     """Entry point for your module."""
     logger = logging.getLogger()
-    x1 = 2
-    y1 = 3
-    logger.info('Realizando suma')
-    logger.debug('{x} + {y} = '.format(x=x1, y=y1) + str(add(x1, y1)))
-    logger.info('Realizando resta')
-    logger.debug('{x} - {y} = '.format(x=x1, y=y1) + str(subtract(x1, y1)))
-    logger.info('Realizando multiplicación')
-    logger.debug('{x} * {y} = '.format(x=x1, y=y1) + str(multiply(x1, y1)))
-    logger.info('Realizando división')
-    logger.debug('{x} / {y} = '.format(x=x1, y=y1) + str(divide(x1, y1)))
-    logger.debug('TERMINADO')
-    logger.critical('FAIL')
 
 
 if __name__ == "__main__":
