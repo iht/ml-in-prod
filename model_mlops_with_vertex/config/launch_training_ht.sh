cc1c4fb4238c534433a5d5aed36139152fd9d094
Israel Herraiz <ihr@google.com>
2022-01-16 22:21:24 +0100

    WIP

--- model_mlops_with_vertex/config/launch_training_ht.sh
+++ model_mlops_with_vertex/config/launch_training_ht.sh
@@ -16,5 +16,7 @@
 #
 gcloud ai hp-tuning-jobs create \
   --region=europe-west4 \
-  --display-name=my-first-model \
+  --display-name=ht-my-first-model \
+  --max-trial-count=56 \
+  --parallel-trial-count=8 \
   --config=./config/training_config_ht.yaml
