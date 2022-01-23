72a968b932138f5f6dfe0b1ca154b97f6ccafd40
Israel Herraiz <ihr@google.com>
2022-01-23 20:48:44 +0100

    Use TF Records as a batched dataset

--- model_mlops_with_vertex/trainer/task.py
+++ model_mlops_with_vertex/trainer/task.py
@@ -17,45 +17,58 @@ import argparse
 import logging
 import os
 import sys
-from typing import Tuple, Optional
+from typing import Optional, List
 
 import hypertune
 import tensorflow as tf
+import tensorflow_transform as tft
 from keras import Model, activations, models, losses, metrics
 from keras import layers
 from keras.layers import TextVectorization
 from keras.optimizer_v2.rmsprop import RMSProp
 from keras.type.types import Layer
-from schemas.imdb_instance import SCHEMA
-
-from . import __version__
+from schemas.imdb_instance import LABEL_COLUMN, TEXT_COLUMN
+from tensorflow_transform import TFTransformOutput
 
 MAX_TOKENS = 20000
 HIDDEN_DIM = 16
 VALIDATION_SPLIT = 0.2
 
+DATASET_SIZE = 25000
+
 NUM_PARALLEL_CALLS = 4  # for performance when transforming text data (assuming 4 vCPUs in worker)
 
 
+def _training_input_fn(file_pattern: List[str],
+                       tf_transform_output: tft.TFTransformOutput,
+                       batch_size: int) -> tf.data.Dataset:
+    transformed_feature_spec = tf_transform_output.transformed_feature_spec()
+
+    dataset: tf.data.Dataset = tf.data.experimental.make_batched_features_dataset(
+        file_pattern=file_pattern,
+        batch_size=batch_size,
+        features=transformed_feature_spec)
+
+    dataset = dataset.map(lambda d: (d[TEXT_COLUMN], d[LABEL_COLUMN]))
+
+    return dataset
+
+
 def _read_tfrecords(data_location: str,
-                    batch_size: int) -> Tuple[tf.data.Dataset, tf.data.Dataset]:  # , tf.data.Dataset]:
-    train_location = os.path.join(data_location, "train/train_data-00000-of-00001.tfrecord")
-    test_location = os.path.join(data_location, "test/test_data-00000-of-00001.tfrecord")
+                    tft_location: str,
+                    batch_size: int) -> (tf.data.Dataset, tf.data.Dataset):
+    train_location = os.path.join(data_location, "train/*")
+    test_location = os.path.join(data_location, "test/*")
 
-    logging.info(f"Reading training data from {train_location}")
-    all_train_ds: tf.data.Dataset = tf.data.TFRecordDataset([train_location])
-    logging.info(f"Reading test data from {test_location}")
-    test_ds: tf.data.Dataset = tf.data.TFRecordDataset([test_location])
+    tf_transform_output: TFTransformOutput = tft.TFTransformOutput(tft_location)
 
-    all_train_ds = all_train_ds.batch(batch_size)
-    all_train_ds = all_train_ds.map(lambda r: tf.io.parse_example(r, SCHEMA))
-    test_ds = test_ds.batch(batch_size)
-    test_ds = test_ds.map(lambda r: tf.io.parse_example(r, SCHEMA))
+    train_ds = _training_input_fn([train_location], tf_transform_output, batch_size=batch_size)
+    test_ds = _training_input_fn([test_location], tf_transform_output, batch_size=batch_size)
 
-    return all_train_ds, test_ds
+    return train_ds, test_ds
 
 
-def get_save_paths(job_dir: Optional[str]):
+def get_save_paths(job_dir: Optional[str]) -> (str, str):
     if job_dir:
         logging.info("Running in local")
         logs_dir = os.path.join(job_dir, "logs")
@@ -66,34 +79,33 @@ def get_save_paths(job_dir: Optional[str]):
         models_dir = os.environ.get('AIP_MODEL_DIR')
 
     logging.info(f"Tensorboard logs will be written to {logs_dir}")
-    logging.info(f"Models written will be to {models_dir}")
+    logging.info(f"Model will be written to {models_dir}")
 
     return logs_dir, models_dir
 
 
 def train_and_evaluate(data_location: str,
+                       tft_location: str,
                        batch_size: int,
                        epochs: int,
+                       dataset_size: int,
                        max_tokens: int,
                        hidden_dim: int,
-                       num_parallel_calls: int,
                        job_dir: Optional[str]):
     logs_dir, models_dir = get_save_paths(job_dir)
 
     train_ds, test_ds = _read_tfrecords(data_location=data_location,
+                                        tft_location=tft_location,
                                         batch_size=batch_size)
 
-    vectorizer: TextVectorization = TextVectorization(ngrams=2, max_tokens=MAX_TOKENS, output_mode="tf_idf")
-    # Build vocabulary
-    vectorizer.adapt(train_ds.map(lambda r: r['text']))
-    train_ds: tf.data.Dataset = train_ds.map(lambda r: (vectorizer(r['text']), r['target']),
-                                             num_parallel_calls=num_parallel_calls)
+    x_text_train = train_ds.map(lambda x, y: x)
 
-    test_ds: tf.data.Dataset = test_ds.map(lambda r: (vectorizer(r['text']), r['target']),
-                                           num_parallel_calls=num_parallel_calls)
+    vectorizer: TextVectorization = layers.TextVectorization(ngrams=2, max_tokens=max_tokens, output_mode="multi_hot")
+    vectorizer.adapt(x_text_train, steps=epochs)
 
-    model = _build_model(max_tokens, hidden_dim)
+    model = _build_model(hidden_dim, vectorizer)
 
+    # Show model summary in log for debugging purposes
     model.summary(print_fn=logging.info)
 
     logging.info(f"Writing TensorBoard logs to {logs_dir}")
@@ -101,7 +113,7 @@ def train_and_evaluate(data_location: str,
         log_dir=logs_dir,
         histogram_freq=1)
 
-    model.fit(train_ds.cache(), epochs=epochs, callbacks=[tensorboard_callback])
+    model.fit(train_ds, epochs=epochs, callbacks=[tensorboard_callback], steps_per_epoch=dataset_size//epochs)
 
     # Evaluate metrics and write to log
     loss, acc = model.evaluate(test_ds)
@@ -118,22 +130,15 @@ def train_and_evaluate(data_location: str,
         global_step=epochs)
 
     # Write model artifact
-    model_path = os.path.join(models_dir, "saved_model")
+    model_path = os.path.join(models_dir)
     logging.info(f"Writing model to {model_path}")
     model.save(model_path)
 
-    # Write text vectorizer (to avoid training-inference drift)
-    vectorizer_path = os.path.join(models_dir, "vectorizer_fn")
-    logging.info(f"Writing text vectorizer to {vectorizer_path}")
-    vectorizer_model = models.Sequential()
-    vectorizer_model.add(layers.Input(shape=(1,), dtype=tf.string))
-    vectorizer_model.add(vectorizer)
-    vectorizer_model.save(vectorizer_path)
 
-
-def _build_model(max_tokens, hidden_dim) -> Model:
-    inputs: Layer = layers.Input(shape=(max_tokens,))
-    x: Layer = layers.Dense(hidden_dim, activation=activations.relu)(inputs)
+def _build_model(hidden_dim, text_vectorizer: TextVectorization) -> Model:
+    inputs: Layer = layers.Input(shape=(1,), dtype=tf.string)
+    x: Layer = text_vectorizer(inputs)
+    x: Layer = layers.Dense(hidden_dim, activation=activations.relu)(x)
     x: Layer = layers.Dropout(0.5)(x)
     outputs: Layer = layers.Dense(1, activation=activations.sigmoid)(x)
     model: Model = models.Model(inputs, outputs, name="my-kschool-model")
@@ -146,11 +151,12 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--data-location', default=None, required=True)
+    parser.add_argument('--tft-location', default=None, required=True)
     parser.add_argument('--epochs', type=int, required=True)
     parser.add_argument('--batch-size', type=int, required=True)
+    parser.add_argument('--full-dataset-size', type=int, default=DATASET_SIZE)
     parser.add_argument('--max-tokens', default=MAX_TOKENS, type=int)
     parser.add_argument('--hidden-dim', default=HIDDEN_DIM, type=int)
-    parser.add_argument('--num-parallel-calls', default=NUM_PARALLEL_CALLS, type=int)
     parser.add_argument('--log', default='INFO', required=False)
     parser.add_argument('--job-dir', default=None, required=False)
 
@@ -160,9 +166,10 @@ if __name__ == "__main__":
     logging.basicConfig(stream=sys.stdout, level=loglevel)
 
     train_and_evaluate(data_location=args.data_location,
+                       tft_location=args.tft_location,
                        epochs=args.epochs,
                        batch_size=args.batch_size,
+                       dataset_size=args.full_dataset_size,
                        max_tokens=args.max_tokens,
                        hidden_dim=args.hidden_dim,
-                       num_parallel_calls=args.num_parallel_calls,
                        job_dir=args.job_dir)
